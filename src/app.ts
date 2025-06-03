304ab90cd8cdb9aa368e4402efdfbb0bb818ab8e
maxim-ld <ayinde779@gmail.com>
2025-06-03 17:13:21 +0100

    fix: remove unused dependency and update app routes

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,6 @@ import authRouter from './routes/authRouter'
 import managerRouter from './routes/managerRouter'
 import adminRouter from './routes/adminRouter'
 import carRouter from './routes/carsRouter'
-import mongoSanitize from 'express-mongo-sanitize'
 
 // an express app instance
 const app = express()
@@ -22,6 +21,12 @@ app.use('/api/v1/admin', adminRouter)
 app.use('/api/v1/customer', customerRouter)
 app.use('/api/v1/manager', managerRouter)
 app.use('/api/v1/cars', carRouter)
+
+app.get('/', (req: Request, res: Response) => {
+    res.status(200).json({
+        message: 'Welcome to Car Dealer API!'
+    })
+})
 
 app.use((req, res) => {
     res.status(404).json({
